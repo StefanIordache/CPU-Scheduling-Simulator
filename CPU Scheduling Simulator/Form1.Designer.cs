eaaeb4a7d1ab2d84997c18a60c6571b72a2ab6c4
Stefan Iordache <traveldevel@hotmail.com>
2018-01-03 22:46:55 +0200

    Add project files.

---
+++ CPU Scheduling Simulator/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿using System.Drawing;
+
+namespace CPU_Scheduling_Simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelInput = new System.Windows.Forms.Panel();
+            this.labelDuration = new System.Windows.Forms.Label();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelPriority = new System.Windows.Forms.Label();
+            this.labelArrivalTime = new System.Windows.Forms.Label();
+            this.textBoxDuration = new System.Windows.Forms.TextBox();
+            this.textBoxPriority = new System.Windows.Forms.TextBox();
+            this.textBoxArrivalTime = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelInput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelInput
+            // 
+            this.panelInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelInput.Controls.Add(this.button1);
+            this.panelInput.Controls.Add(this.textBoxArrivalTime);
+            this.panelInput.Controls.Add(this.textBoxPriority);
+            this.panelInput.Controls.Add(this.textBoxDuration);
+            this.panelInput.Controls.Add(this.labelArrivalTime);
+            this.panelInput.Controls.Add(this.labelPriority);
+            this.panelInput.Controls.Add(this.labelDuration);
+            this.panelInput.Controls.Add(this.labelInput);
+            this.panelInput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelInput.Location = new System.Drawing.Point(12, 12);
+            this.panelInput.Name = "panelInput";
+            this.panelInput.Size = new System.Drawing.Size(301, 252);
+            this.panelInput.TabIndex = 0;
+            // 
+            // labelDuration
+            // 
+            this.labelDuration.AutoSize = true;
+            this.labelDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDuration.Location = new System.Drawing.Point(3, 58);
+            this.labelDuration.Name = "labelDuration";
+            this.labelDuration.Size = new System.Drawing.Size(93, 25);
+            this.labelDuration.TabIndex = 1;
+            this.labelDuration.Text = "Duration";
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInput.Location = new System.Drawing.Point(96, 0);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(83, 32);
+            this.labelInput.TabIndex = 0;
+            this.labelInput.Text = "Input";
+            // 
+            // labelPriority
+            // 
+            this.labelPriority.AutoSize = true;
+            this.labelPriority.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPriority.Location = new System.Drawing.Point(3, 143);
+            this.labelPriority.Name = "labelPriority";
+            this.labelPriority.Size = new System.Drawing.Size(79, 25);
+            this.labelPriority.TabIndex = 2;
+            this.labelPriority.Text = "Priority";
+            this.labelPriority.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelArrivalTime
+            // 
+            this.labelArrivalTime.AutoSize = true;
+            this.labelArrivalTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelArrivalTime.Location = new System.Drawing.Point(3, 101);
+            this.labelArrivalTime.Name = "labelArrivalTime";
+            this.labelArrivalTime.Size = new System.Drawing.Size(128, 25);
+            this.labelArrivalTime.TabIndex = 3;
+            this.labelArrivalTime.Text = "Arrival Time";
+            // 
+            // textBoxDuration
+            // 
+            this.textBoxDuration.Location = new System.Drawing.Point(166, 58);
+            this.textBoxDuration.Name = "textBoxDuration";
+            this.textBoxDuration.Size = new System.Drawing.Size(86, 22);
+            this.textBoxDuration.TabIndex = 4;
+            // 
+            // textBoxPriority
+            // 
+            this.textBoxPriority.Location = new System.Drawing.Point(166, 143);
+            this.textBoxPriority.Name = "textBoxPriority";
+            this.textBoxPriority.Size = new System.Drawing.Size(86, 22);
+            this.textBoxPriority.TabIndex = 5;
+            // 
+            // textBoxArrivalTime
+            // 
+            this.textBoxArrivalTime.Location = new System.Drawing.Point(166, 101);
+            this.textBoxArrivalTime.Name = "textBoxArrivalTime";
+            this.textBoxArrivalTime.Size = new System.Drawing.Size(86, 22);
+            this.textBoxArrivalTime.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(81, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Add to list";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1690, 897);
+            this.Controls.Add(this.panelInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CPU Scheduling SImulator";
+            this.panelInput.ResumeLayout(false);
+            this.panelInput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelInput;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelDuration;
+        private System.Windows.Forms.Label labelArrivalTime;
+        private System.Windows.Forms.Label labelPriority;
+        private System.Windows.Forms.TextBox textBoxArrivalTime;
+        private System.Windows.Forms.TextBox textBoxPriority;
+        private System.Windows.Forms.TextBox textBoxDuration;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
