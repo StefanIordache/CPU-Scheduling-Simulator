6b917681eb4aa4a401eb50f9fb19f265464aa8c8
Stefan Iordache <traveldevel@hotmail.com>
2018-01-03 23:55:00 +0200

    Added Class Models (Process + Scheduler)

---
+++ CPU Scheduling Simulator/Models/Process.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_Scheduling_Simulator.Models
+{
+    public enum ProcessStatus
+    {
+        NotStarted = -1,
+        Waiting = 0,
+        OnCpu = 1,
+        Finished = 2    
+    }
+
+    public class Process
+    {
+        public int Id { get; set; }
+
+        public int Order { get; set; }
+
+        public int ArrivalTime { get; set; }
+
+        public int Duration { get; set; }
+
+        public int RemainingDuration { get; set; }
+
+        public int FinishTime { get; set; }
+
+        public int WaitingTime { get; set; }
+
+        public bool Finished { get; set; }
+
+        public int Priority { get; set; }
+
+        public double CompletedPercentage { get; set; }
+
+        public ProcessStatus Status { get; set; }
+    }
+}
---
+++ CPU Scheduling Simulator/Models/Scheduler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_Scheduling_Simulator.Models
+{
+    public class Scheduler
+    {
+        public SchedulerAlgorithm Algorithm { get; set; }
+
+        public SchedulerStatus Status { get; set; }
+
+        public CpuUtilization CpuUtilization { get; set; }
+
+        public int CurrentTime { get; set; }
+
+        public double AverageWaitingTime { get; set; }
+
+        public double AverageTurnaroundTime { get; set; }
+
+        public double AverageResponseTime { get; set; }
+
+        public virtual ICollection<Process> ProcesesList { get; set; }
+
+        public virtual ICollection<Process> ProcessesQueue { get; set; }
+    }
+
+    public enum SchedulerAlgorithm
+    {
+        FCFS = 1, //First Come First Serve
+        RR = 2 //Round Robin
+    }
+
+    public enum SchedulerStatus
+    {
+        On = 0,
+        Off = 1
+    }
+
+    public enum CpuUtilization
+    {
+        No = 0,
+        Yes = 1
+    }
+
+
+}
